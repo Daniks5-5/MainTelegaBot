91c5c27dc9090b203164ee66b126cf555741e36a
Danik <elik@example.com>
2023-09-21 16:00:04 +0300

    all cod without log

---
+++ src/main/java/com/bot/telega/config/BotInitializer.java
@@ -0,0 +1,33 @@
+package com.bot.telega.config;
+
+import com.bot.telega.service.TelegramBot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Component
+public class BotInitializer { //класс для инициализации бота
+
+    @Autowired
+    TelegramBot bot;
+
+    @EventListener({ContextRefreshedEvent.class})
+    public void init() throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        try {
+            telegramBotsApi.registerBot(bot);
+
+        } catch (TelegramApiException e) { // здесь будут логи
+
+
+
+        }
+
+
+    }
+}
--- src/main/java/com/bot/telega/service/TelegramBot.java
+++ src/main/java/com/bot/telega/service/TelegramBot.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
@@ -21,13 +22,24 @@ public class TelegramBot extends TelegramLongPollingBot {
     public void onUpdateReceived(Update update) { //главный центральный метод, здесь обработка того что пишет пользователь и возращается ответ
         if (update.hasMessage() && update.getMessage().hasText()) {                 //проверка на сообщение от пользователя и проверка что там есть текст
             String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId(); // получение id  чата для отправки сообщений ботом
+            long chatId = update.getMessage().getChatId(); // получение id  чата для отправки сообщений ботом, при отправке сообщений ChatId типа long
 
 
 
             switch (messageText){
                 case "/start":
-                    startCommandReceived(chatId, update.getMessage(). getChat(). getFirstName()); //getFirstName отвечает за получения имени человека
+                    try {
+                        startCommandReceived(chatId, update.getMessage(). getChat(). getFirstName()); //getFirstName отвечает за получения имени человека
+                        break;
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                default:
+                    try {
+                        sendMessage(chatId, "Сорри брат, пока еще не поддерживается такой функционал");
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
 
             }
 
@@ -35,13 +47,21 @@ public class TelegramBot extends TelegramLongPollingBot {
         }
 
         }
-    private void startCommandReceived(long chatId, String name){
+    private void startCommandReceived(long chatId, String name) throws TelegramApiException {
         String answer = "Привет, "+name+", рад тебя видеть здесь :)";
+        sendMessage(chatId, answer);
 
     }
-    private void sendMessage(long chatId, String textToSend){
+    private void sendMessage(long chatId, String textToSend) throws TelegramApiException {
         SendMessage message = new SendMessage(); // класс SendMessage класс для отправки сообщений боту
-        message.setChatId(chatId);// чтобы что-то отправить нужен ChatId
+        message.setChatId(String.valueOf(chatId));// чтобы что-то отправить нужен ChatId, при входящем сообщение ChatId типа String
+        message.setText(textToSend);
+
+        try{
+            execute(message);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
